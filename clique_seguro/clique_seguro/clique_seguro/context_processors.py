57b2dda47780950b40fdcf6e2ec55eac9c579299
Polyana Pinheiro Ferreira <polyanaferreira1236@gmail.com>
2026-06-08 16:59:13 -0400

    Delete clique_seguro directory

--- clique_seguro/clique_seguro/clique_seguro/context_processors.py
+++
@@ -1,8 +0,0 @@
-def contrast_processor(request):
-    """
-    Verifica na sessão do utilizador se o alto contraste está ativado.
-    Envia a variável 'high_contrast' automaticamente para todos os HTMLs.
-    """
-    return {
-        'high_contrast': request.session.get('high_contrast', False)
-    }
